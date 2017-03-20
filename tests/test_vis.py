87d8d2c4ca5b26161a94df88be729e49fefef55a
Bart Römgens <bart.romgens@gmail.com>
2017-03-20 11:38:17 +0100

    create booleans to reuse/create data and plot log/linear

--- tests/test_vis.py
+++ tests/test_vis.py
@@ -13,40 +13,46 @@ import vis.data
 
 class TestCountour(unittest.TestCase):
     YEAR = 2010
+    recreate = False
+    logscale = True
 
     def test_create_contour(self):
         config = vis.emissioncontour.ContourPlotConfig()
-        # sources = enipedia.euets.get_all_sources_from_file(year=self.YEAR)
-        sources = enipedia.euets.get_sources_from_file(country_code='NL', year=self.YEAR)
+        sources = enipedia.euets.get_all_sources_from_file(year=self.YEAR)
+        # sources = enipedia.euets.get_sources_from_file(country_code='NL', year=self.YEAR)
         contour = vis.emissioncontour.Contour(emission_sources=sources, config=config)
-        contour.create_contour_data()
-        contour.save()
-        # contour.load()
+        if TestCountour.recreate:
+            contour.create_contour_data()
+            contour.save()
+        else:
+            contour.load()
         level_upper = contour.Z.max()
         level_lower = contour.Z.min()
         print(level_upper)
         print(level_lower)
 
-        n_contours = 11
-        level_lower = 500
-        level_upper /= 2
-        # norm = SymLogNorm(linthresh=1.0, vmin=level_lower, vmax=level_upper)
-        # norm = PowerNorm(gamma=2, vmin=level_lower, vmax=level_upper)
-        norm = None
-        levels = numpy.logspace(
-            start=math.log(level_lower),
-            stop=math.log(level_upper + 2),
-            num=n_contours,
-            base=math.e
-        )
-        levels = numpy.linspace(
-            start=level_lower,
-            stop=level_upper,
-            num=n_contours
-        )
+        n_contours = 16
+        level_lower = 10
+        level_upper /= 1
+        if TestCountour.logscale:
+            norm = SymLogNorm(linthresh=1.0, vmin=level_lower, vmax=level_upper)
+            # norm = PowerNorm(gamma=2, vmin=level_lower, vmax=level_upper)
+            levels = numpy.logspace(
+                start=math.log(level_lower),
+                stop=math.log(level_upper + 2),
+                num=n_contours,
+                base=math.e
+            )
+        else:
+            norm = None
+            levels = numpy.linspace(
+                start=level_lower,
+                stop=level_upper,
+                num=n_contours
+            )
         print(levels)
         # norm=None
-        # contour.create_contour_plot(levels=levels, norm=norm)
+        contour.create_contour_plot(levels=levels, norm=norm)
         contour.create_geojson('emissions.geojson', levels=levels, norm=norm)
 
 
